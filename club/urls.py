c151e393bdd57e0118fa885da8cfec425dc11ac4
kalosius <kalosius43@gmail.com>
2023-09-07 11:59:33 +0300

    Nav links created successfully

--- club/urls.py
+++ club/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('matches/', views.matches, name='matches'),
+    path('news/', views.news, name='news'),
+    path('partners/', views.partners, name='partners'),
+    path('players/', views.players, name='players'),
+    path('stadium/', views.stadium, name='stadium'),
 ]
--- club/views.py
+++ club/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def matches(request):
+    return render(request, 'matches.html')
+
+
+def news(request):
+    return render(request, 'news.html')
+
+
+def partners(request):
+    return render(request, 'partners.html')
+
+
+def players(request):
+    return render(request, 'players.html')
+
+
+def stadium(request):
+    return render(request, 'stadium.html')
